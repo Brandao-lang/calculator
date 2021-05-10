cf1ab03affc2981d5e2aff73f21647833d6365fb
Brandao-lang <elijahbrandao@gmail.com>
2021-05-10 15:04:34 -0400

    current

--- calc.js
+++ calc.js
@@ -14,6 +14,35 @@ reader.question("What would you like to calculate?", function(input){
 	num1 = Number(tokens[1]);
 	num2 = Number(tokens[2]);
 
+	if (mathSymbol == "+") {
+		let sum = num1 + num2
+		console.log("the answer is " + sum)
+	}
+
+	else if (mathSymbol == "-") {
+		let diff = num1 - num2
+		console.log("the answer is " + diff)
+	}
+
+	else if (mathSymbol == "*") {
+		let multi = num1 * num2
+		console.log("the answer is " + multi)
+	}
+
+	else if (mathSymbol =="/") {
+		let divide = num1/num2
+		console.log("the answer is " + divide)
+	}
+
+	else if (mathSymbol = "root") {
+		squareRoot = Math.sqrt(num1)
+		console.log("the answer is " + squareRoot)
+	}
+
+
+
+
+
 
 	// This line closes the connection to the command line interface.
 	reader.close()
