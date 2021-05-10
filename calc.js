4687308425b94a02d2fc8560169bfd33958d0727
Brandao-lang <elijahbrandao@gmail.com>
2021-05-10 15:43:18 -0400

    added final study

--- calc.js
+++ calc.js
@@ -34,9 +34,31 @@ reader.question("What would you like to calculate?", function(input){
 		console.log("the answer is " + divide)
 	}
 
-	else if (mathSymbol = "root") {
+	else if (mathSymbol == "root") {
 		squareRoot = Math.sqrt(num1)
 		console.log("the answer is " + squareRoot)
+	}
+
+	else if (mathSymbol == "square") {
+		squared = Math.pow(num1, 2)
+		console.log("the answer is " + squared)
+		
+	}
+	
+	else if (mathSymbol == "cube") {
+		cubed = Math.pow(num1, 3)
+		console.log("the answer is " + cubed)
+		
+	}
+
+	else if (mathSymbol == "raise") {
+		raised = Math.pow(num1, num2)
+		console.log(`${num1} raised to ${num2} is ${raised}`)
+	}
+
+	else if (mathSymbol == "remain") {
+		remainder = num1 % num2
+		console.log("the remainder is " + remainder)
 	}
 
 
